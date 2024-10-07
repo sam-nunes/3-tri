bdcddf7cc295f2639199eb4f75e0e5b250c26145
Nunes <102034901+sam-nunes@users.noreply.github.com>
2024-10-07 13:34:12 +0000

    Alterações

--- graficos/Quantidadeusuarios.js
+++ graficos/Quantidadeusuarios.js
@@ -1,24 +1,19 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-
-    const data = [
-        {
+async function quantidaedeUsuariosPorRede() {
+  const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+  const res = await fatch(url)
+  const dados = await res.json()
+  const nomeDasRedes = Object.keys(dados)
+  const quantidadeDeUsuarios = Object.values(dados)
+  const data = [
+      {
           x: nomeDasRedes,
-          y: quantidadeUsuarios,
+          y:quantidadeDeUsuarios,
           type: 'bar'
-        }
-      ]
-      const grafico = document.createElement('div')
-      grafico.className = 'grafico'
-      document.getElementById('graficos-container').appendChild(grafico)
-      Plotly.newPlot(grafico, data)
-
-
-
-  }
-
-  quantidadeUsuarios()
+      }
+  ]
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementById('garfico-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data)
+}
+quantidaedeUsuariosPorRede()
